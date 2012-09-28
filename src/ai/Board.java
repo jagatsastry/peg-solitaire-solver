af6fdeaafbafb48cecd543f6c01d9ae2999e9598
jagatsastry <jagatsastry@gmail.com>
2012-09-28 17:25:18 -0400

    commit

--- src/ai/Board.java
+++ src/ai/Board.java
@@ -4,24 +4,90 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import ai.Board.SlotState;
 
 
 public class Board {
     
-	public static enum SlotState { INVALID, EMPTY, PEG};
+	public Hole get(int i, int j) {
+		return m_board[i][j];
+	}
 	
-	private SlotState[][] m_board;
+	@Override public String toString() {
+		StringBuilder build = new StringBuilder();
+		for(Hole[] row: m_board)
+			build.append(Arrays.toString(row) + "\n");
+		return build.toString();
+	}
+	
+	public static enum Hole {
+		
+		INVALID('-', -1),
+		EMPTY('0', 0),
+		PEG('X', 1);
+		
+		private char m_inpchar;
+		private int m_bit;
+		
+		private Hole(char inpchar, int bit) {
+			m_inpchar = inpchar;
+			m_bit = bit;
+		}
+		
+		static Hole fromChar(char c) {
+			for(Hole s: values()) 
+				if(s.getInpChar() == c)
+					return s;
+			return null;
+		}
+
+		private char getInpChar() {
+			// TODO Auto-generated method stub
+			return m_inpchar;
+		}
+		
+		@Override public String toString() {
+			return String.valueOf(m_inpchar);
+		}
+		
+		private int bit() {
+			assert m_bit != -1;
+			return m_bit;
+		}
+	};
+	
+	private Hole[][] m_board;
+	
+	public long bitMap() {
+		long bitMap = 0; 
+		for(int i = 0; i < SIZE; i++) {
+			for(int j = 0; j < SIZE; j++)
+				if(m_board[i][j] != Hole.INVALID)
+					bitMap = ((bitMap<<1) | m_board[i][j].bit());
+		}
+		return bitMap;
+	}
+	
+	public List<Long> getSymmetricConfigs() { 
+		//TODO: Perform rotations and reflections
+		List<Long> retVal = new ArrayList<Long>();
+		retVal.add(bitMap());
+		return retVal;
+//		
+//		for(int i = 0; i < SIZE; i++) 
+//			for(int j = 0; j < SIZE; j++) {
+//              				
+//			}
+	}
+	
 	public static final int SIZE = 7;
 	
-	private Board(SlotState[][] slots) {
+	private Board(Hole[][] slots) {
 		m_board = slots;
 	}
 	
-	public static Board inputBoard() throws IOException {
-		Board board = new Board(new SlotState[7][7]);
+    public static Board inputBoard() throws IOException {
 		List<String> boardList = new ArrayList<String>(7);
 		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
 		for(int i = 0; i < SIZE; i ++) {
@@ -29,21 +95,33 @@ public class Board {
 				boardList.add(buff.readLine());
 			}
 		}
-		board.setBoard(boardList);
 		
-		return board;
+		return getBoard(boardList);
 	}
 
-	private void setBoard(List<String> boardList) {
+	public static Board getBoard(List<String> boardList) {
 		// TODO Auto-generated method stub
+		Hole[][] arrayBoard = new Hole[SIZE][SIZE];
 		for(int i = 0; i < SIZE ; i++) {
 			char[] row = boardList.get(i).toCharArray();
 			for(int j = 0; j < SIZE; j++) {
-				m_board[i][j] = SlotState.fromChar(row[j]);
+				arrayBoard[i][j] = Hole.fromChar(row[j]);
 			}
 		}
-				
-		
+		return new Board(arrayBoard);
+	}
+
+	//Assumption: The move has been verified as valid
+	public void move(int x, int y, int dx, int dy) {
+		m_board[x][y] = Hole.EMPTY;
+		m_board[x + dx/2][y + dy/2] = Hole.EMPTY;
+		m_board[x + dx][y + dy] = Hole.PEG;
+	}
+
+	public void restore(int x, int y, int dx, int dy) {
+		m_board[x + dx][y + dy] = Hole.EMPTY;
+		m_board[x + dx/2][y + dy/2] = Hole.PEG;
+		m_board[x][y] = Hole.PEG;
 	}
 
 }
--- src/ai/PegSolitaireSolver.java
+++ src/ai/PegSolitaireSolver.java
@@ -3,26 +3,127 @@
  */
 package ai;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+import ai.Board.Hole;
 
 /**
  * @author jagat
  *
  */
 public class PegSolitaireSolver {
-     
-	Board board;
+    
+	public static class Move {
+		int m_fromx, m_fromy, m_tox, m_toy;
+
+		public Move(int fromx, int fromy, int tox, int toy) {
+			super();
+			this.m_fromx = fromx;
+			this.m_fromy = fromy;
+			this.m_tox = tox;
+			this.m_toy = toy;
+		}
+
+		public void printMove() {
+			System.out.printf("Move (%d, %d) to (%d, %d)%n", m_fromx, m_fromy, m_tox, m_toy);
+		}
+	}
 	
+	private Board m_board;
+	private Set<Long> m_unsolvableStates = new HashSet<Long>();
+	private Stack<Move> m_moves = new Stack<Move>();
+	public PegSolitaireSolver(Board board) {
+		m_board = board;
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Board initialConfig = Board.inputBoard();
-        
+		try {
+			List<List<String>> boardConfigs = getTests();
+			for(List<String> cfg: boardConfigs) {
+				Board testBoard = Board.getBoard(cfg);
+				PegSolitaireSolver solver = new PegSolitaireSolver(testBoard);
+				if(!solver.solve())
+					System.err.println("The given game instance cannot be solved");
+				else solver.printSteps();
+			}
+		    
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
+	private static List<List<String>> getTests() {
+		List<List<String>> tests = new ArrayList<List<String>>();
+		List<String> test = new ArrayList<String>() { 
+			private static final long serialVersionUID = 1L;
+			{add("--000--"); } 
+			{add("--0X0--"); } 
+	        {add("00XXX00"); } 
+	        {add("000X000"); }
+	        {add("000X000"); }
+	        {add("--000--"); }
+	        {add("--000--"); }
+        }; 
 		
+		tests.add(test);
+		return tests;
+	}
 
+	private void printSteps() {
+        while(!m_moves.empty())
+        	m_moves.pop().printMove();
+	}
+    
+	int[][] deltas = new int[][]{{-2, 0}, {2, 0}, {0, -2}, {0, 2}};
+	
+			
+	private boolean solve() {
+		int pegCount = 0;
+		for(int x = 0; x < Board.SIZE; x++) {
+			for(int y = 0; y < Board.SIZE; y++) {
+				if(this.m_board.get(x, y) == Hole.PEG) {
+					pegCount++; 
+					for(int[] delta: deltas) {
+						int dx = delta[0];
+						int dy = delta[1];
+						if(validMove(x, y, dx, dy)) {
+							m_board.move(x, y, dx, dy);
+							Long boardCfg = m_board.bitMap();
+							if(!m_unsolvableStates.contains(boardCfg)) {
+								if(solve()) {
+									m_moves.push(new Move(x, y, x + dx, y + dy));
+									return true;
+								}
+								m_unsolvableStates.add(boardCfg);
+							}
+							m_board.restore(x, y, dx, dy);
+						}
+					}
+				}
+			}
+		}
+		return pegCount == 1;
+	}
+
+	private boolean validMove(int x, int y, int dx, int dy) {
+		int stepx = x + dx/2, stepy = y + dy/2,
+				jumpx = x + dx, jumpy = y + dy;
+		if(stepx < 0 || stepx >= Board.SIZE || stepy < 0 || stepy >= Board.SIZE 
+				|| jumpx < 0 || jumpx >= Board.SIZE || jumpy < 0 || jumpy >= Board.SIZE)
+			return false;
+
+		Hole intHole = m_board.get(stepx,  stepy);
+        Hole destHole = m_board.get(jumpx, jumpy);
+        if(intHole == Hole.INVALID || intHole == Hole.EMPTY || 
+        		destHole == Hole.INVALID || destHole != Hole.EMPTY)
+        	return false;
+        return true;
 	}
 }
